f88e724e12772be6f28018dd21d27ea733e31011
imaniclarke4 <imaniclarke97@gmail.com>
2021-10-10 16:35:41 -0500

    tic-tac-toe.js

---
+++ tic-tac-toe.js
@@ -0,0 +1,17 @@
+"use strict";
+window.onload = function(){
+    var element = document.getElementById("board").children;
+
+    for (var i= 0; i<element.length; i++){
+        element[i].classList.add('.square');
+    }
+    
+}
+
+
+
+
+
+
+
+
